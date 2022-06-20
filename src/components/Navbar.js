d9f4fa6e73d2820b953edae6807c15f61095299f
Ayush Poojari <ayushp.b11@gmail.com>
2022-06-20 13:46:28 +0530

    Added Logo Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import logo from './images/logo.png'
 
 function NavigationPane() {
   return (
     <>      
     <Navbar bg="light">
-        <Container>
+        <Container>          
           <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
           </Navbar.Brand>
             <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="gallery">Gallery</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="contact">Contact Us</Nav.Link>
             </Nav>
         </Container>
     </Navbar>
